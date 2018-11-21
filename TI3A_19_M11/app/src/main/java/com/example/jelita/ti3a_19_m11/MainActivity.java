52d7abfa1b9ab27b34058bb7383214774a6751e8
jelitasm <43837893+jelitasm@users.noreply.github.com>
2018-11-21 13:56:46 +0700

    init

---
+++ TI3A_19_M11/app/src/main/java/com/example/jelita/ti3a_19_m11/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jelita.ti3a_19_m11;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ TI3A_19_M11/app/src/main/java/com/example/jelita/ti3a_19_m11/Rest/ApiClient.java
@@ -0,0 +1,24 @@
+package com.example.jelita.ti3a_19_m11.Rest;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+    public static final String BASE_URL = "http://192.168.71.13/rest_server/";
+    private static Retrofit retrofit = null;
+    public static Retrofit getClient() {
+        if (retrofit==null) {
+            Gson gson = new GsonBuilder()
+                    .setLenient()
+                    .create();
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build();
+        }
+        return retrofit;
+    }
+}
